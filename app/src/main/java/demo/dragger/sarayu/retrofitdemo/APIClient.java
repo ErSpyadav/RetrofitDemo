6f650d2548faecc27e11b45df043f3b9308194d7
SarayuYadaw <sarayu264@gmail.com>
2018-12-10 17:55:00 +0530

    Initial commit

---
+++ app/src/main/java/demo/dragger/sarayu/retrofitdemo/APIClient.java
@@ -0,0 +1,29 @@
+package demo.dragger.sarayu.retrofitdemo;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class APIClient {
+
+    private static Retrofit retrofit = null;
+
+    static Retrofit getClient() {
+
+        /*This is used to log string of Responce*/
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        /*Gson is used to convert Responce into Poso*/
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://reqres.in")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit;
+    }
+
+}
